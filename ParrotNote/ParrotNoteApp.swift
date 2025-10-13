66ec208717edbb4472633cc4225eeb0cc6c91905
inan-mahmud <inan.mahmud.dev@gmail.com>
2025-10-13 20:13:10 +0600

    Initial Commit

---
+++ ParrotNote/ParrotNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParrotNoteApp.swift
+//  ParrotNote
+//
+//  Created by cefalo on 13/10/25.
+//
+
+import SwiftUI
+
+@main
+struct ParrotNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
